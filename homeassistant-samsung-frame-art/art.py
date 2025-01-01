e532c9a3936de17b68c6f4f75e00bf2aab9ba363
Exploramus <nil>
2025-01-01 17:49:50 +0100

    Introduced StateData for state persistance, including LastContentID

--- homeassistant-samsung-frame-art/art.py
+++ homeassistant-samsung-frame-art/art.py
@@ -7,11 +7,37 @@ import logging
 import argparse
 from PIL import Image, ImageOps
 
-
-sys.path.append('../')
-
 from samsungtvws.async_art import SamsungTVAsyncArt
 from samsungtvws import exceptions
+
+class StateData:
+    def __init__(self, last_content_id=None, uploaded_photos=None):
+        self.LastContentID = last_content_id
+        self.Uploaded_Photos = uploaded_photos if uploaded_photos is not None else []
+
+    def to_dict(self):
+        return {
+            "LastContentID": self.LastContentID,
+            "Uploaded_Photos": self.Uploaded_Files
+        }
+
+    @classmethod
+    def from_dict(cls, data):
+        return cls(
+            last_content_id=data.get("LastContentID"),
+            uploaded_photos=data.get("Uploaded_Files", [])
+        )
+
+def load_state_data(filepath):
+    if os.path.exists(filepath):
+        with open(filepath, 'r') as file:
+            data = json.load(file)
+            return UploadData.from_dict(data)
+    return UploadData()
+
+def save_state_data(filepath, upload_data):
+    with open(filepath, 'w') as file:
+        json.dump(upload_data.to_dict(), file)
 
 
 def parseargs():
@@ -25,10 +51,11 @@ def parseargs():
     return parser.parse_args()
     
 
-
 async def main():
     # Set log level
     logging.basicConfig(level=logging.INFO) #or logging.DEBUG to see messages
+
+    sys.path.append('../')
 
     # Parse command line parameters
     args = parseargs()
@@ -42,22 +69,21 @@ async def main():
             folder_path = os.path.join(folder_path, args.subfolder)
     logging.info('Folder is (2): {}'.format(folder_path))
 
-    # Set the path to the file containing the last used images
-    uploaded_json_path = '/data/uploaded.json'
+    # Set the path to the file containing the state of the addon 
+    state_json_path = '/data/frame_switcher_state.json'
 
-    # Load the list of last 5 uploaded pictures
-    if os.path.exists(uploaded_json_path):
-        with open(uploaded_json_path, 'r') as file:
-            uploaded_photos = json.load(file)
-    else:
-        uploaded_photos = []
-
+    # Load the state of the addon (last 5 uploaded pictures & last content ID)
+    frame_state = load_state_data(state_json_path)
+    if not frame_state:
+        frame_state = StateData()
+    logging.info('frame addon state: {}'.format(frame_state))
+    
     # Get the list of all photos in the folder
     files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
 
     if len(files) > 5:
         # Exclude the photos on the uploaded list
-        available_files = [f for f in files if f not in uploaded_photos]
+        available_files = [f for f in files if f not in frame_state.Uploaded_Photos]
 
         # Select a random photo from the available files
         selected_photo = random.choice(available_files)
@@ -87,29 +113,36 @@ async def main():
    
     if supported:
         try:
-            #is tv on (calls tv rest api)
+            # is tv on (calls tv rest api)
             tv_on = await tv.on()
             logging.info('tv is on: {}'.format(tv_on))
             
-            #is art mode on
-            #art_mode = await tv.get_artmode()                  #calls websocket command to determine status
-            art_mode = tv.art_mode                              #passive, listens for websocket messgages to determine art mode status
+            # is art mode on
+            # art_mode = await tv.get_artmode()                  # calls websocket command to determine status
+            art_mode = tv.art_mode                              # passive, listens for websocket messages to determine art mode status
             logging.info('art mode is on: {}'.format(art_mode))
 
-            #get current artwork
+            # get current artwork
             current = await tv.get_current()
             logging.info('current artwork: {}'.format(current))
-            current_content_id = current['content_id']
+            current_content_id = current.get('content_id', None)
+            if current_content_id is None:
+                logging.error('Current artwork does not have a content_id')
+                return
 
             photos = [f for f in os.listdir(folder_path) if f.lower().endswith(('.png', '.jpg'))]
             if not photos:
                 logging.info('No PNG or JPG photos found in the folder')
                 return
             else:
-                filename = selected_photo
-                filename = os.path.join(folder_path, filename)
+                selected_photo = photos[0]  # Assuming selected_photo is the first photo in the list
+                filename = os.path.join(folder_path, selected_photo)
                 new_filename = os.path.join(folder_path, os.path.basename(filename).lower())
-                os.rename(filename, new_filename)
+                try:
+                    os.rename(filename, new_filename)
+                except FileNotFoundError:
+                    logging.error('File not found: {}'.format(filename))
+                    return
                 filename = new_filename
                 logging.info('Selected and renamed photo: {}'.format(filename))
 
@@ -133,18 +166,15 @@ async def main():
                
                     #delete the file that was showing before if uploaded through this script
                     #checking if current artwork was uploaded through this script
-                    if current in uploaded_photos:
+                    if current_content_id == frame_state.LastContentID:
                         await tv.delete_list([current_content_id])
                         logging.info('deleted from tv: {}'.format([current_content_id]))  
 
-                    uploaded_photos.append(selected_photo)
-                    if len(uploaded_photos) > 5:
-                        uploaded_photos.pop(0)
+                    frame_state.Uploaded_Photos.append(selected_photo)
+                    if len(frame_state.Uploaded_Photos.) > 5:
+                        frame_state.Uploaded_Photos..pop(0)
                     
-                    with open(uploaded_json_path, 'w') as file:
-                        json.dump(uploaded_photos, file)
-
-
+                    save_state_data(state_json_path,frame_state)
 
             await asyncio.sleep(15)
 
